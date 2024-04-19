cbe8bf6e368f453d2bccd902e82e68cb2f24c2d5
Trcmoe <88249678+Trcmoe@users.noreply.github.com>
2024-04-19 23:09:57 +0800

    Fix link

--- src/components/WhitePantyhoseFooter.vue
+++ src/components/WhitePantyhoseFooter.vue
@@ -4,8 +4,8 @@
       <v-img :width="200" src="@/assets/fontlogo-white.webp"></v-img>
       <p class="white-pantyhose-footer-year">CC-BY-NC-SA-4.0 2020 - {{ new Date().getFullYear() }}</p>
       <div class="white-pantyhose-footer-left-links">
-        <v-btn variant="plain" size="small" target="_blank" href="https://github.com/Arcomua-Team/">Github</v-btn>
-        <v-btn variant="plain" size="small" target="_blank" href="https://github.com/Arcomua-Team/ArcoKita-Website">Source</v-btn>
+        <v-btn variant="plain" size="small" target="_blank" href="https://github.com/Arcomua/">Github</v-btn>
+        <v-btn variant="plain" size="small" target="_blank" href="https://github.com/Arcomua/ArcoKita-Website">Source</v-btn>
       </div>
     </div>
     <div class="white-pantyhose-footer-right">
